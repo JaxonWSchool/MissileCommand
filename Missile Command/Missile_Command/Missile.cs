26a10e42bfad96584c83ff3c77a3cbb659c1386f
JaxonWSchool <153121366+JaxonWSchool@users.noreply.github.com>
2023-12-08 09:03:31 -0600

    Hadent Day 1 Starter Code

---
+++ Missile Command/Missile_Command/Missile.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Missile_Command
+{
+    class Missile
+    {
+        public Rectangle rect;
+        public int rotation;
+        public Texture2D texture;
+        public Vector2 target;
+    }
+}
---
+++ Missile Command/Missile_Command/Round.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Missile_Command
+{
+    class Round
+    {
+        public Round()
+        {
+
+        }
+    }
+}
