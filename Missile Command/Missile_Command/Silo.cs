7f58f0d96387fd921e2e754a4f68eb1d167492d7
JaxonWSchool <153121366+JaxonWSchool@users.noreply.github.com>
2023-12-12 00:54:46 -0600

    Update Silo.cs

--- Missile Command/Missile_Command/Silo.cs
+++ Missile Command/Missile_Command/Silo.cs
@@ -7,9 +7,32 @@ namespace Missile_Command
 {
     class Silo
     {
-        public Silo()
+        public Rectangle rect;
+        public Missile missile;
+        public List<Rectangle> ammo;
+        public Silo(Rectangle rect)
         {
+            this.rect = rect;
+            missile = null;
+            ammo = new List<Rectangle>();
 
+            int width = 10;
+            int height = 15;
+            //draw ammo in a pyramid
+            ammo.Add(new Rectangle(rect.X, rect.Y, width, height));
+
+            ammo.Add(new Rectangle(rect.X - width / 2, rect.Y + height, width, height));
+            ammo.Add(new Rectangle(rect.X + width / 2, rect.Y + height, width, height));
+
+            ammo.Add(new Rectangle(rect.X - width, rect.Y + height * 2, width, height));
+            ammo.Add(new Rectangle(rect.X, rect.Y + height * 2, width, height));
+            ammo.Add(new Rectangle(rect.X + width, rect.Y + height * 2, width, height));
+
+            ammo.Add(new Rectangle(rect.X - width * 3 / 2, rect.Y + height * 3, width, height));
+            ammo.Add(new Rectangle(rect.X - width * 1 / 2, rect.Y + height * 3, width, height));
+            ammo.Add(new Rectangle(rect.X + width * 1 / 2, rect.Y + height * 3, width, height));
+            ammo.Add(new Rectangle(rect.X + width * 3 / 2, rect.Y + height * 3, width, height));
         }
+
     }
 }
